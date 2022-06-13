cbf14f272665b097507201b4267e6005e84dd91f
Miha <luksic.miha@gmail.com>
2022-06-14 00:14:27 +0200

    Implement garbage automation Lambda function

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module garbage-automation
 
 go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.32.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twilio/twilio-go v0.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/joho/godotenv"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
 
 func main() {
-	fmt.Println("it works")
+	lambda.Start(garbageAutomation)
+}
+
+func garbageAutomation() {
+	garbagePickups, err := readFile("garbage.csv")
+
+	if err != nil {
+		log.Fatal("There was an error reading CSV file")
+	}
+
+	currentDate := time.Now()
+	for _, p := range garbagePickups {
+		date, err := time.Parse("2006-01-02", p[1])
+
+		if err != nil {
+			log.Fatal("There was an issue parsing CSV date into object")
+		}
+
+		if currentDate.Day()+1 == date.Day() && currentDate.Month() == date.Month() {
+			sendSMS(p[0])
+		}
+	}
+}
+
+func readFile(csvFile string) ([][]string, error) {
+	f, err := os.Open(csvFile)
+
+	if err != nil {
+		log.Fatal("There was an error reading CSV file")
+	}
+
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	if _, err := r.Read(); err != nil {
+		log.Fatal("there was an error reading first line in CSV file")
+		return [][]string{}, err
+	}
+
+	garbagePickups, err := r.ReadAll()
+
+	if err != nil {
+		log.Fatal("There was an error reading all CSV values")
+		return [][]string{}, err
+	}
+
+	return garbagePickups, nil
+}
+
+func sendSMS(msg string) {
+
+	env := os.Getenv("APP_ENV")
+
+	if env == "" || env == "development" {
+		err := godotenv.Load(".env.local")
+		if err != nil {
+			log.Fatal("There was an error loading .env file")
+		}
+	}
+
+	accountSid := os.Getenv("ACCOUNT_SID")
+	authToken := os.Getenv("AUTH_TOKEN")
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(os.Getenv("PHONE_NUMBER"))
+	params.SetBody(msg + " kanta")
+	params.SetMessagingServiceSid(os.Getenv("SERVICE_ID"))
+	resp, err := client.Api.CreateMessage(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		response, _ := json.Marshal(*resp)
+		fmt.Println("Response: " + string(response))
+	}
 }
